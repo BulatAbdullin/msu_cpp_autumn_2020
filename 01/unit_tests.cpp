5798905c3ae66fba86995a0fd722e0919eac7326
Bulat Abdullin <bulat.abdullin@stud.cs.msu.ru>
2020-10-13 18:07:09 +0300

    Added alloc_max_size() and reset() unit tests.

--- 01/unit_tests.cpp
+++ 01/unit_tests.cpp
@@ -19,3 +19,22 @@ void alloc_just_right()
 	strcpy(ptr, message);
 	assert(strcmp(ptr, message) == 0);
 }
+
+void alloc_max_size()
+{
+	Allocator allocator;
+	allocator.makeAllocator(1473);
+	assert(allocator.alloc(1473) == allocator.getPtrStart());
+}
+
+void reset()
+{
+	Allocator allocator;
+	allocator.makeAllocator(99);
+	char *ptr = allocator.alloc(10);
+	assert(ptr == allocator.getPtrStart());
+	ptr = allocator.alloc(50);
+	assert(ptr != allocator.getPtrStart());
+	allocator.reset();
+	assert(allocator.getPtrCurrent() == allocator.getPtrStart());
+}
