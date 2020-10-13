120ac9098d6d0416c254b98683932bc12c2de371
Bulat Abdullin <bulat.abdullin@stud.cs.msu.ru>
2020-10-13 22:55:50 +0300

    Fixed memory leak in Allocator::makeAllocator if called twice

--- 01/allocator.cpp
+++ 01/allocator.cpp
@@ -9,8 +9,16 @@ Allocator::Allocator()
 
 void Allocator::makeAllocator(size_t max_size)
 {
+	// Let's reuse the already allocated memory if we have enough space
+	// and reallocate it if a greater max_size is required.
+	if (max_size > this->max_size) {
+		if (ptr_start) {
+			delete[] ptr_start;
+		}
+		ptr_start = new char[max_size];
+	}
 	unused_size = this->max_size = max_size;
-	ptr_start = ptr_current = new char[max_size];
+	ptr_current = ptr_start;
 }
 
 char *Allocator::alloc(size_t size)
